6f3f06225e82f73a7fc5a898723431a295fc8f9b
ehandresen <eirik.horgen.andresen@gmail.com>
2024-05-08 15:51:03 +0200

    added delete method, and installed react-toastify

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,10 +1,25 @@
-import { useParams, useLoaderData } from 'react-router-dom';
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
+
+  function onDeleteClick(jobId) {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this listing?'
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate('/jobs');
+  }
 
   return (
     <>
@@ -81,7 +96,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
